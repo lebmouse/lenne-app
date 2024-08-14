8fcb7a1de56145426dcd01a795523784ac8f8dcd
lebmouse <gtah2yk@gmail.com>
2024-08-14 22:08:39 +0900

    feat: setup typia

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import unTypiaNext from "@ryoppippi/unplugin-typia/next";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const config = {
+  // your next.js config
+};
+export default unTypiaNext(
+  config,
+  {} // options of unplugin-typia
+);
